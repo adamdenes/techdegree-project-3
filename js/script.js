4dec24d298a8f3ba84c1b8c9d1bd7d1e18b014c2
Adam Denes <adam.denes@protonmail.com>
2019-12-21 11:28:05 +0100

    Adding T-shirt section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-/******************************************
+/** ****************************************
+
 Treehouse Techdegree:
+
 FSJS project 3 - Interactive Form
-******************************************/
+
+***************************************** */
 
 // ADD THIS LATER!
 // document.addEventListener('DOMContentLoaded', () => {});
@@ -12,13 +15,59 @@ $('#name').focus();
 
 // hide the 'other-title' job role input field
 $('#other-title').hide();
-// show input field once 'Other' is selected from dropdown menu
-$('select').on('change', function() {
-    // if we select anything but 'other', hide it
-    $(this).val() === 'other' ? $('#other-title').show() : $('#other-title').hide();
+
+// // show input field once 'Other' is selected from dropdown menu
+$('#title').on('change', function() {
+  // if we select anything but 'other', hide it
+  $(event.target).val() === 'other'
+    ? $('#other-title').show()
+    : $('#other-title').hide();
 });
 
 // T-Shirt section
-// $('#design').hide();
-// $('#size').hide()
-// $('#color').text('');
+$('#design option')
+  .eq(0)
+  .attr('hidden', true);
+
+$('#design option')
+  .eq(1)
+  .attr('selected', true);
+
+// update the 'color' field to read "Please select a T-shirt theme"
+const $warning = $('<option>Please select a T-shirt theme</option>');
+$('#color').prepend($warning.attr('selected', true).attr('disabled', true));
+// hide the colors in the 'color' drop down menu
+$('#color option').hide();
+// use 'change' event listener on 'design' menu 'select' element
+$('#design').on('change', function(event) {
+  // if 'js puns' is selected hide all 'heart js' option in 'color'
+  // if 'heart js' is selected hide all 'js puns' option in 'color'
+  // update the 'color' field to the first available color
+  if ($(event.target).val() === 'js puns') {
+    $('#color option').each(function(i) {
+      if (
+        $(this)
+          .text()
+          .includes('Puns')
+      ) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  }
+
+  if ($(event.target).val() === 'heart js') {
+    $('#color option').each(function(i) {
+      if (
+        $(this)
+          .text()
+          .includes('♥')
+      ) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  }
+});
