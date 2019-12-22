73a7f45fcaf1ef78f4204f1f68f03abdf2a27305
Adam Denes <adam.denes@protonmail.com>
2019-12-22 12:11:23 +0100

    Finishing 'T-shirt' section

--- js/script.js
+++ js/script.js
@@ -36,19 +36,17 @@ $('#design option')
 // update the 'color' field to read "Please select a T-shirt theme"
 const $warning = $('<option>Please select a T-shirt theme</option>');
 $('#color').prepend($warning.attr('selected', true).attr('disabled', true));
+
 // hide the colors in the 'color' drop down menu
 $('#color option').hide();
-// use 'change' event listener on 'design' menu 'select' element
-$('#design').on('change', function(event) {
-  // if 'js puns' is selected hide all 'heart js' option in 'color'
-  // if 'heart js' is selected hide all 'js puns' option in 'color'
-  // update the 'color' field to the first available color
-  if ($(event.target).val() === 'js puns') {
-    $('#color option').each(function(i) {
+
+function handleOptions(eventTarget, value, object, text) {
+  if ($(eventTarget).val() === value) {
+    $(object).each(function() {
       if (
         $(this)
           .text()
-          .includes('Puns')
+          .includes(text)
       ) {
         $(this).show();
       } else {
@@ -56,18 +54,34 @@ $('#design').on('change', function(event) {
       }
     });
   }
+}
 
-  if ($(event.target).val() === 'heart js') {
-    $('#color option').each(function(i) {
-      if (
-        $(this)
-          .text()
-          .includes('♥')
-      ) {
-        $(this).show();
-      } else {
-        $(this).hide();
-      }
-    });
+function addSelected(color, index) {
+  return $(color)
+    .eq(index)
+    .attr('selected', true);
+}
+
+function removeSelected(color, index) {
+  return $(color)
+    .eq(index)
+    .removeAttr('selected', false)
+    .attr('hidden', true);
+}
+
+// use 'change' event listener on 'design' menu 'select' element
+$('#design').on('change', function(event) {
+  // if 'js puns' is selected hide all 'heart js' option in 'color'
+  // if 'heart js' is selected hide all 'js puns' option in 'color'
+  // update the 'color' field to the first available color
+
+  if ($(event.target).val() === 'js puns') {
+    handleOptions(event.target, 'js puns', '#color option', 'Puns');
+    addSelected('#color option', 1);
+    removeSelected('#color option', 4);
+  } else {
+    handleOptions(event.target, 'heart js', '#color option', '♥');
+    addSelected('#color option', 4);
+    removeSelected('#color option', 1);
   }
 });
