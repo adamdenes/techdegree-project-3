027ff44304accc156b8777479654b579cd85f767
Adam Denes <adam.denes@protonmail.com>
2019-12-23 11:01:01 +0100

    Finishing 'activities' section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/** ****************************************
+/* *****************************************
 
 Treehouse Techdegree:
 
@@ -9,7 +9,9 @@ FSJS project 3 - Interactive Form
 // ADD THIS LATER!
 // document.addEventListener('DOMContentLoaded', () => {});
 
-// Other section
+/* *****************************************
+”Job Role” section
+***************************************** */
 // focus 'name' input on page load
 $('#name').focus();
 
@@ -24,7 +26,9 @@ $('#title').on('change', function() {
     : $('#other-title').hide();
 });
 
-// T-Shirt section
+/* *****************************************
+”T-Shirt Info” section
+***************************************** */
 $('#design option')
   .eq(0)
   .attr('hidden', true);
@@ -65,23 +69,69 @@ function addSelected(color, index) {
 function removeSelected(color, index) {
   return $(color)
     .eq(index)
-    .removeAttr('selected', false)
-    .attr('hidden', true);
+    .removeAttr('selected', false);
 }
 
 // use 'change' event listener on 'design' menu 'select' element
 $('#design').on('change', function(event) {
   // if 'js puns' is selected hide all 'heart js' option in 'color'
-  // if 'heart js' is selected hide all 'js puns' option in 'color'
-  // update the 'color' field to the first available color
-
   if ($(event.target).val() === 'js puns') {
     handleOptions(event.target, 'js puns', '#color option', 'Puns');
+    // update the 'color' field to the first available color
     addSelected('#color option', 1);
     removeSelected('#color option', 4);
+    // if 'heart js' is selected hide all 'js puns' option in 'color'
   } else {
     handleOptions(event.target, 'heart js', '#color option', '♥');
+    // update the 'color' field to the first available color
     addSelected('#color option', 4);
     removeSelected('#color option', 1);
   }
 });
+
+/* *****************************************
+”Register for Activities” section
+***************************************** */
+
+// create an element to display the total activity cost
+let cost = 0;
+const $costHTML = $('<span></span>');
+$('.activities').append($costHTML);
+
+// listen for changes in the Activity section
+$('.activities').on('change', function(event) {
+  const $checkbox = $(event.target);
+  const $dataCost = parseInt($(event.target).attr('data-cost'));
+  const $dataDate = $(event.target).attr('data-day-and-time');
+
+  // if the checkbox is checked, add the cost declared in the 'data-cost' to 'cost' variable
+  // otherwise subtract it
+  $checkbox.prop('checked') ? (cost += $dataCost) : (cost -= $dataCost);
+
+  // printing out the total cost to the page dynamically
+  $costHTML.text(`Total: $${cost}`);
+
+  // looping over the input fields and set the date to a variable
+  $('.activities input').each(function() {
+    const $inputDate = $(this).attr('data-day-and-time');
+
+    if (
+      $dataDate === $inputDate && // if the date attributes are the same
+      $checkbox !== $(this) && // but they are not the same activity
+      $dataDate !== undefined // and there is an actual date attribute
+    ) {
+      // plus the checkbox is checked
+      if ($checkbox.prop('checked')) {
+        // disable the conflicting input tag
+        $(this).attr('disabled', true);
+        // otherwise let it be enabled
+      } else {
+        $(this).attr('disabled', false);
+      }
+      // remove disabled attribute from the clicked element so it can be modified later
+      $checkbox.removeAttr('disabled', false);
+    }
+  });
+});
+
+// Disable conflicting activities
